9e5438264672f53af3340a57e9908c01bc451c0f
loliland11231 <125196041+loliland11231@users.noreply.github.com>
2025-11-27 19:27:38 +0400

    ddddddddddddddddddddddddddddddddddddddddddd

--- w.py
+++ w.py
@@ -1,1 +1,49 @@
-print('?')
+# print('?')
+
+
+
+
+#
+# def i_bolno_v_noge():
+#     s = input('Напиши 6 скобок.')
+#     if s[0] == ')' :
+#         print('no')
+#         return ''
+#     elif s[0] == '(':
+#         if s[5] == ')':
+#             print('Yes')
+#             return ''
+#         else:
+#             print('no')
+#             return ''
+#     else:
+#         print('no')
+#         return ''
+#     if s[1] == ')':
+#         print('no')
+#         return ''
+#     elif s[1] == '(':
+#         if s[4] == ')':
+#             print('Yes')
+#             return ''
+#         else:
+#             print('no')
+#             return ''
+#     if s[2] == ')':
+#         print('no')
+#         return ''
+#     elif s[2] == '(':
+#         if s[3] == ')':
+#             print('Yes')
+#             return ''
+#         else:
+#             print('no')
+#             return ''
+# i_bolno_v_noge()
+
+
+
+
+
+
+
